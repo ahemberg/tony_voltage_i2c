5f55eb940b43f5638b419a192994fe408b469361
Alexander Hemberg <alexander.hemberg@gmail.com>
2018-12-01 14:07:24 +0100

    changed to simple working demo

--- tiny_voltage_i2c.ino
+++ tiny_voltage_i2c.ino
@@ -2,30 +2,50 @@
 #include <twi.h>
 
 /*
- * Simple sketch that configures an attiny85 as I2c Slave. The tiny measures  
+ * Simple sketch that configures an attiny85 as I2c Slave. The tiny measures
  * voltage on one of its pins continously and sends it off to the I2c Master
  * when requested.
- * 
+ *
  * Uses TinyWire.h
- * 
+ *
  */
 
 #define I2C_SLAVE_ADDR  0x26
 #define VOLTAGE_PIN A2
 
-uint8_t voltage_adc;
-uint8_t received = 0;
+union Uint16AsBytes {
+  uint16_t ival;
+  uint8_t bval[2];
+};
+
+uint16_t voltage_adc;
+bool sending = false;
+uint8_t byte_counter = 0;
+
+Uint16AsBytes voltage_un;
 
 void setup() {
   pinMode(VOLTAGE_PIN, INPUT);
+
   TinyWire.begin(I2C_SLAVE_ADDR);
+  //TinyWire.onReceive(set_mode); TODO: Implement maybe? Request different ADCs
   TinyWire.onRequest(send_data);
 }
 
 void loop() {
-  voltage_adc = analogRead(VOLTAGE_PIN)/4; 
+  if (!sending) {
+    voltage_adc = analogRead(VOLTAGE_PIN);
+    voltage_un.ival = voltage_adc;
+  }
 }
 
 void send_data() {
-  TinyWire.send(voltage_adc);
+    bool sending = true;
+    TinyWire.send(voltage_un.bval[byte_counter]);
+    byte_counter++;
+
+    if (byte_counter >= 2) {
+      byte_counter = 0;
+      sending = false;
+    }
 }
